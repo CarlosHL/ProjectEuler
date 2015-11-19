ad89eb26d6866f7fde393a728f67a2b475d4cfe9
Carlos Herrero Lozano <100290969@alumnos.uc3m.es>
2015-11-18 23:56:44 -0700

    adding problem 5

---
+++ ex5/Problem5.java
@@ -0,0 +1,35 @@
+package euler;
+
+public class Problem5 {
+
+	/**
+	 *20=2^2*5
+	 *19
+	 *18=2*3^2
+	 *17
+	 *16=2^4
+	 *15=3*5
+	 *14=7*2
+	 *13
+	 *12=2^2*3
+	 *11
+	 *10=2*5
+	 *9=3^2
+	 *8=2^3
+	 *7
+	 *6=2*3
+	 *5
+	 *4=2^2
+	 *3
+	 *2
+	 *1
+	 *COMUNES Y NO COMUNES CON SUS MAYORES EXPONENTES
+	 */
+	
+	public static void main(String[] args) {
+		int result=2*2*2*2*3*3*5*7*11*13*17*19;
+		System.out.println(result);
+
+	}
+
+}
